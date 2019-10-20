62d3cf8dfdc6e769eccc4abdf778d6a89e4b8013
Fares Tabet <fares_tabet@hotmail.fr>
2019-10-20 13:45:45 -0700

    implemented csv to array

--- Sorting/src/helpers/CSVHelper.java
+++ Sorting/src/helpers/CSVHelper.java
@@ -1,9 +1,50 @@
 package helpers;
 
-public class CSVHelper
-{
-    public static void main(String[] args)
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVHelper {
+
+    public static void main(String[] args) throws IOException
     {
+
+        System.out.println(Arrays.toString(CSVToArray("src/csv/MOCK_DATA.txt")));
+
+    }
+
+    public static double[] CSVToArray(String filepath) throws FileNotFoundException, IOException
+    {
+        //Create arraylist of doubles
+        List<Double> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filepath)))
+        {
+            String line;
+
+            while ((line = br.readLine()) != null)
+            {
+                String[] values = line.split(",");
+                for(int i = 0;i < values.length;i++)
+                {
+                    records.add(Double.parseDouble(values[i]));
+                }
+
+            }
+
+            //return array of doubles
+            return records.stream().mapToDouble(i -> i).toArray();
+
+        }catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
+
+        return null;
 
     }
 }
